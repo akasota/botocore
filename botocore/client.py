33acba24d137fd7f485d300975a3bb5973f6a608
akasota <lopes.lucasaranha@gmail.com>
2022-03-06 13:12:28 -0300

    Update client.py

--- botocore/client.py
+++ botocore/client.py
@@ -10,6 +10,10 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+
+import tensorflow_io as tfio
+import tensorflow as tf
+
 import logging
 
 from botocore import waiter, xform_name
